3f43e1c9b83526d456890618cd0ce25b1f5fd44d
jafar khadour <jkhadour111@gmail.com>
2025-06-08 17:13:13 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,26 +1,27 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
 #include <vector>
 #include <memory>
-#include <algorithm>
 
 struct TreeNode {
     char value;
     std::vector<std::shared_ptr<TreeNode>> children;
-    TreeNode(char val) : value(val) {}
+    TreeNode(char v) : value(v) {}
 };
 
 class PMTree {
  private:
     std::shared_ptr<TreeNode> root;
-    void buildTree(std::shared_ptr<TreeNode> node, std::vector<char> elements);
+    void buildTree(std::shared_ptr<TreeNode> node, const std::vector<char>& elements);
     void getAllPermsHelper(std::shared_ptr<TreeNode> node, std::vector<char>& current,
                          std::vector<std::vector<char>>& result) const;
-    void getPermByNumHelper(std::shared_ptr<TreeNode> node, int& remaining, 
-                          std::vector<char>& result) const;
-    bool getPermByNumFastHelper(std::shared_ptr<TreeNode> node, int remaining,
-                              std::vector<char>& result, int& total) const;
+    void getPermHelper1(std::shared_ptr<TreeNode> node, std::vector<char>& current,
+                      int& target, int& currentNum, std::vector<char>& result) const;
+    bool getPermHelper2(std::shared_ptr<TreeNode> node, std::vector<char>& current,
+                      int& remaining, std::vector<char>& result) const;
+    int factorial(int n) const;
 
  public:
     explicit PMTree(const std::vector<char>& elements);
@@ -30,7 +31,7 @@ class PMTree {
 };
 
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
-std::vector<char> getPerm1(PMTree& tree, int num);
-std::vector<char> getPerm2(PMTree& tree, int num);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
